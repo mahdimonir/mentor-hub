26af770d657d405154c333e780c42fd6c801d77d
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-06-03 19:49:30 +0600

    feat(auth): implement sign-in and sign-up views with form validation
    - Added `SignInView` and `SignUpView` components for user authentication.
    - Integrated form handling using `react-hook-form` and `zod` for validation.
    - Created a new `Layout` component for consistent styling across auth pages.
    - Updated `package.json` to include `better-auth` for authentication management.
    - Added API routes for authentication using `better-auth`.
    - Created database schema for user, session, account, and verification tables.
    - Introduced a new logo SVG for branding.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,98 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
-  return <Button>Click Me</Button>;
+  const { data: session } = authClient.useSession();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async () => {
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Something went wrong, please try again.");
+        },
+        onSuccess: () => {
+          window.alert("User created successfully!");
+          setName("");
+          setEmail("");
+          setPassword("");
+        },
+        onLoading: () => {
+          window.alert("Creating user...");
+        },
+      }
+    );
+  };
+
+  const onLogin = async () => {
+    authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Something went wrong, please try again.");
+        },
+        onSuccess: () => {
+          window.alert("User login successfully!");
+          setEmail("");
+          setPassword("");
+        },
+        onLoading: () => {
+          window.alert("Creating user...");
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Sign out</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input
+        placeholder="name"
+        value={name}
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        type="email"
+        autoComplete="email"
+        autoFocus
+        required
+        pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        title="Please enter a valid email address."
+        aria-label="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={onSubmit}>Create User</Button>
+    </div>
+  );
 }
